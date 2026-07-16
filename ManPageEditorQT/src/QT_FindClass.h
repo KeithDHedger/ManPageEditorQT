44243c45d72d1deccd0a74b1f1fbeb5f523e045f
K D Hedger <kdhedger68713@gmail.com>
2026-07-16 15:04:32 +0100

    Added find

--- ManPageEditorQT/src/QT_FindClass.h
+++ ManPageEditorQT/src/QT_FindClass.h
@@ -1,3 +1,22 @@
+/*
+ *
+ * ©K. D. Hedger. Thu 16 Jul 15:03:16 BST 2026 keithdhedger@gmail.com
+
+ * This file (QT_FindClass.h) is part of ManPageEditorQT.
+
+ * ManPageEditorQT is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * ManPageEditorQT is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with ManPageEditorQT.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #ifndef _QT_FINDCLASS_
 #define _QT_FINDCLASS_
